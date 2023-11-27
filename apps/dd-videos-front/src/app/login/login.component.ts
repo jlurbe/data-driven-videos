bc8c379f13b6f7ff594c11f4a9dc51a8053acec7
JuanJo Lurbe <juan.lurbe_escrihuela@contractors.roche.com>
2023-11-27 17:04:16 +0100

    feat: google login

--- apps/dd-videos-front/src/app/login/login.component.ts
+++ apps/dd-videos-front/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthGoogleService } from '../auth-google.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  login() {}
+  constructor(private authGoogleService: AuthGoogleService) {
+    this.authGoogleService.initLogin();
+  }
+
+  login() {
+    this.authGoogleService.login();
+  }
 }
--- apps/dd-videos-front/src/app/main/main.component.ts
+++ apps/dd-videos-front/src/app/main/main.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthGoogleService } from '../auth-google.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {}
+export class MainComponent {
+  constructor(private authGoogleService: AuthGoogleService) {
+    this.authGoogleService.initLogin();
+  }
+
+  getProfile() {
+    return this.authGoogleService.getProfile();
+  }
+
+  logout() {
+    this.authGoogleService.logout();
+  }
+}
